0d7014fdb70ed590f22ad08cc245f97de12051ff
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-10-04 17:55:13 -0300

    feat: implement products module

---
+++ src/products/products.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) { }
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.getAllProducts();
+  }
+
+  @Get('spotlights')
+  getProductsSpotlights() {
+    return this.productsService.getProductsSpotlights();
+  }
+
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductsService {
+
+  private products = [
+    {
+      id: 1,
+      item: 'Jersey Los Angeles Angels',
+      description: 'Jersey number 17 - Shohei Otani for Los Angeles Angels',
+      sku: 123456,
+      price: 189.55,
+      quantity: 10,
+      color: [
+        'red',
+        'grey',
+        'white',
+      ],
+      spotlight: false,
+    },
+  ];
+
+  getAllProducts() {
+    return this.products;
+  }
+
+  getProductsSpotlights() {
+    const listSpotlights = this.products.filter(product => product.spotlight);
+
+    return listSpotlights;
+  }
+
+}
